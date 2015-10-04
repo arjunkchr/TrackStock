25198a44ac3bebff6539b2fa2dc7507e745d2dfd
Arjun Kochhar <arjun.ice@gmail.com>
2015-10-04 17:06:10 +0530

    Fixed an exception thrown while enountering nil data in background fetch

--- TrackStock/AppDelegate.swift
+++ TrackStock/AppDelegate.swift
@@ -14,10 +14,10 @@ let backgroundFetchInterval: NSTimeInterval = 5
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil))
@@ -33,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let dataUrl: NSURL? = NSURL(string: url)
             
             let session = NSURLSession.sharedSession().dataTaskWithURL(dataUrl!, completionHandler: { (data, response, error) -> Void in
+                if data != nil
+                {
                     let datastring = NSString(data: data!, encoding: NSUTF8StringEncoding) as! String
                     let csv = CSwiftV(String: datastring)
                     print("\(csv.headers[1])")
@@ -46,39 +48,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                         localNotification.alertBody = "Stock:" + key + " Set Limit:" + stockTrack[key]! + " Current Value:" + csv.headers[1]
                         localNotification.fireDate = NSDate(timeIntervalSinceNow: 0)
                         UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+                    }
                 }
-
+                
             })
             session.resume()
         }
         completionHandler(UIBackgroundFetchResult.NewData)
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
         print("applicationWillResignActive")
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         print("applicationDidEnterBackground")
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         print("applicationWillTerminate")
     }
-
-
+    
+    
 }
-
